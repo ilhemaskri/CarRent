912fde3510307cd0c205b9fe041b60f1faaf59fd
ilhemaskri <ilhem.askri@hotmail.de>
2021-01-25 19:13:50 +0100

    First Commit CarRent Project

---
+++ source/src/CarRent/CustomerManagement/Infrastructure/CustomerRepository.cs
@@ -0,0 +1,36 @@
+﻿namespace CarRent.CustomerManagement.Infrastructure
+{
+    using CarRent.CustomerManagement.Domain;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public class CustomerRepository : ICustomerRepository
+    {
+        public void Add(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer FindById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Customer> FindByName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
